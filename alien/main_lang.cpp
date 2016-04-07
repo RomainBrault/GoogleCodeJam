f4453c11896df2e2d380e1a3bac47bb976d21ac7
RomainBrault <ro.brault@gmail.com>
2016-04-07 10:36:39 +0200

    Initial commit

---
+++ alien/main_lang.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <regex>
+
+#define CHECK_STREAM(S) do { \
+    if (!(S)) throw std::ios_base::failure("error while parsing file"); \
+} while (0)
+
+#define CHECK_END_STREAM(S) do { \
+    file.ignore(1); \
+    if (!file.eof()) throw std::ios_base::failure("error while parsing file"); \
+} while (0)
+
+class AlienData {
+private:
+    size_t _L;
+    size_t _D;
+    size_t _N;
+    std::vector<std::string> _alien_words;
+    std::vector<std::string> _alien_signals;
+
+public:
+
+public:
+    auto parse_file(const std::string & filename)
+    {
+
+        std::ifstream file;
+        file.open(filename);
+
+        CHECK_STREAM(file >> _L);
+        CHECK_STREAM(file >> _D);
+        CHECK_STREAM(file >> _N);
+
+        _alien_words = std::vector<std::string>(D());
+        _alien_signals = std::vector<std::string>(N());
+
+        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        for (size_t i = 0; i < D(); ++i) {
+            CHECK_STREAM(std::getline(file, _alien_words[i], '\n'));
+        }
+        for (size_t i = 0; i < N(); ++i) {
+            CHECK_STREAM(std::getline(file, _alien_signals[i], '\n'));
+            bool mod = false;
+            for (size_t j = 0; j < alien_signal(i).size(); ++j) {
+                if (alien_signal(i)[j] == '(') {
+                    mod = true; continue;
+                }
+                else if (alien_signal(i)[j] == ')') {
+                    mod = false; continue;
+                }
+                else if (mod && alien_signal(i)[j+1] != ')') {
+                    alien_signal(i).insert(++j, "|"); continue;
+                }
+            }
+        }
+        CHECK_END_STREAM(file);
+        file.close();
+
+        return *this;
+    }
+
+    inline auto L(void) const -> size_t
+    {
+        return _L;
+    }
+
+    inline auto D(void) const -> size_t
+    {
+        return _D;
+    }
+
+    inline auto N(void) const -> size_t
+    {
+        return _N;
+    }
+
+    inline auto alien_word(size_t i) -> std::string &
+    {
+        return _alien_words[i];
+    }
+
+    inline auto alien_signal(size_t i) -> std::string &
+    {
+        return _alien_signals[i];
+    }
+
+};
+
+int main(int argc, char** argv) {
+
+    if (argc < 3) {
+        std::cerr << "expected \'./programe filename\' n_threads ";
+    }
+
+    AlienData data;
+    data.parse_file(argv[1]);
+
+    std::vector<size_t> acc(data.N());
+#pragma omp parallel for num_threads(atoi(argv[2]))
+    for (size_t i = 0; i < data.N(); ++i) {
+        std::regex alien_signal_regex(data.alien_signal(i),
+            std::regex::optimize | std::regex_constants::nosubs);
+        for (size_t j = 0; j < data.D(); ++j) {
+            acc[i] += std::regex_match(data.alien_word(j), alien_signal_regex);
+        }
+    }
+    for (size_t i = 0; i < data.N(); ++i) {
+        std::cout << "Case #" + std::to_string(i + 1) + ": "
+                  << acc[i] << std::endl;
+    }
+
+    return 0;
+}
---
+++ alien/main_num.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <vector>
+#include <ios>
+#include <algorithm>
+
+#define CHECK_STREAM(S) do { \
+    if (!(S)) throw std::ios_base::failure("error while parsing file"); \
+} while (0)
+
+#define CHECK_END_STREAM(S) do { \
+    file.ignore(1); \
+    if (!file.eof()) throw std::ios_base::failure("error while parsing file"); \
+} while (0)
+
+class AlienData {
+private:
+    size_t _n_num;
+    std::vector<std::string> _alien_number;
+    std::vector<std::string> _source_language;
+    std::vector<std::string> _target_language;
+
+public:
+    auto parse_file(const std::string & filename) {
+
+        std::ifstream file;
+        file.open(filename);
+
+        CHECK_STREAM(file >> _n_num);
+
+        _alien_number = std::vector<std::string>(n_num());
+        _source_language = std::vector<std::string>(n_num());
+        _target_language = std::vector<std::string>(n_num());
+
+        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        for (size_t i = 0; i < n_num(); ++i) {
+            CHECK_STREAM(std::getline(file, _alien_number[i], ' '));
+            CHECK_STREAM(std::getline(file, _source_language[i], ' '));
+            CHECK_STREAM(std::getline(file, _target_language[i], '\n'));
+
+        }
+        CHECK_END_STREAM(file);
+
+        file.close();
+
+        return *this;
+    }
+
+    inline auto n_num(void) const
+        -> size_t
+    {
+        return _n_num;
+    }
+
+    inline auto source_base(size_t i)
+        -> size_t
+    {
+        return source_language(i).size();
+    }
+
+    inline auto target_base(size_t i)
+        -> size_t
+    {
+        return target_language(i).size();
+    }
+
+    inline auto alien_number(size_t i)
+        -> std::string &
+    {
+        return _alien_number[i];
+    }
+
+    inline auto source_language(size_t i)
+        -> std::string &
+    {
+        return _source_language[i];
+    }
+
+    inline auto target_language(size_t i)
+        -> std::string &
+    {
+        return _target_language[i];
+    }
+
+    inline auto alien_number_to_source_base(size_t i)
+        -> std::vector<size_t>
+    {
+        std::vector<size_t> res(alien_number(i).size() + 1);
+        size_t j = 0;
+        res[j++] = source_base(i);
+        for (char c : alien_number(i)) {
+            res[j++] = source_language(i).find(c);
+        }
+        return res;
+    }
+
+    inline auto target_base_to_alien_number(
+            const std::vector<size_t> & target_base, size_t i)
+        -> std::string
+    {
+        std::string res;
+        for (size_t j = 1; j < target_base.size(); ++j) {
+            res += target_language(i).at(target_base[j]);
+        }
+        return res;
+    }
+};
+
+inline auto source_base_to_decimal(const std::vector<size_t> & source_base)
+    -> size_t
+{
+    size_t base = source_base[0];
+    size_t acc = 0;
+    for (size_t i = 1; i < source_base.size(); ++i) {
+        acc *= base;
+        acc += source_base[i];
+    }
+    return acc;
+}
+
+inline auto decimal_to_target_base(size_t dec, size_t target_base)
+    -> std::vector<size_t>
+{
+    std::vector<size_t> res;
+    res.push_back(target_base);
+    do {
+        res.push_back(dec % target_base);
+        dec /= target_base;
+    } while (dec != 0);
+    std::reverse(res.begin() + 1, res.end());
+    return res;
+}
+
+int main(int argc, char** argv) {
+
+    if (argc < 3) {
+        std::cerr << "expected \'./programe filename\' n_threads ";
+    }
+
+    AlienData data;
+    data.parse_file(argv[1]);
+
+    std::vector<std::string> res(data.n_num());
+#pragma omp parallel num_threads(std::atoi(argv[2]))
+{
+    std::vector<size_t> t1;
+    std::vector<size_t> t2;
+#pragma omp for
+    for (size_t i = 0; i < data.n_num(); ++i) {
+        t1 = data.alien_number_to_source_base(i);
+        size_t dec = source_base_to_decimal(t1);
+        t2 = decimal_to_target_base(dec, data.target_base(i));
+        res[i] = data.target_base_to_alien_number(t2, i);
+    }
+}
+    for (size_t i = 0; i < data.n_num(); ++i) {
+            std::cout << "Case #" + std::to_string(i + 1) + ": "
+                      << res[i] << std::endl;
+    }
+    return 0;
+}
